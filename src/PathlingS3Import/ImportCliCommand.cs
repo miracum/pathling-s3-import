9f439255c027bbd157ed06310b10163d7943c09d
chgl <chgl@users.noreply.github.com>
2024-02-24 16:01:19 +0100

    feat: improved logging

--- src/PathlingS3Import/ImportCliCommand.cs
+++ src/PathlingS3Import/ImportCliCommand.cs
@@ -93,6 +93,10 @@ public class ImportCliCommand
             .AddRetry(retryOptions) // Add retry using the default options
             .Build(); // Builds the resilience pipeline
 
+        log.LogInformation(
+            "Pathling FHIR  base URL set to {PathlingBaseUrl}",
+            PathlingServerBaseUrl
+        );
         using var fhirClient = new FhirClient(
             PathlingServerBaseUrl,
             settings: new FhirClientSettings
@@ -104,6 +108,7 @@ public class ImportCliCommand
             }
         );
 
+        log.LogInformation("Minio endpoint set to {S3Endpoint}", S3Endpoint);
         using var minio = new MinioClient()
             .WithEndpoint(S3Endpoint)
             .WithCredentials(S3AccessKey, S3SecretKey)
@@ -118,6 +123,12 @@ public class ImportCliCommand
         ResiliencePipeline retryPipeline
     )
     {
+        log.LogInformation(
+            "Checking if bucket {S3BucketName} exists in {S3BaseUrl}.",
+            S3BucketName,
+            minio.Config.BaseUrl
+        );
+
         var bucketExistsArgs = new BucketExistsArgs().WithBucket(S3BucketName);
         bool found = await minio.BucketExistsAsync(bucketExistsArgs);
         if (!found)
@@ -139,16 +150,22 @@ public class ImportCliCommand
             .WithPrefix(prefix)
             .WithRecursive(false);
 
-        var observable = minio.ListObjectsAsync(listArgs);
+        var observable =
+            minio.ListObjectsAsync(listArgs)
+            ?? throw new InvalidOperationException("observable for listing buckets is null");
 
         var allObjects = new List<Minio.DataModel.Item>();
 
-        using (log.BeginScope("[listing objects]"))
+        using (log.BeginScope("[Listing objects in {ObjectsPath}]", $"{S3BucketName}/{prefix}"))
         {
             using var subscription = observable.Subscribe(
                 item =>
                 {
-                    log.LogInformation("Listing object: {ItemKey} ({IsDir})", item.Key, item.IsDir);
+                    log.LogInformation(
+                        "Got object: {ItemKey} (IsDir: {IsDir})",
+                        item.Key,
+                        item.IsDir
+                    );
                     if (!item.IsDir && item.Key.EndsWith(".ndjson"))
                     {
                         allObjects.Add(item);
@@ -161,18 +178,26 @@ public class ImportCliCommand
             observable.Wait();
         }
 
+        log.LogInformation("Found a total of {ObjectCount} matching objects.", allObjects.Count);
+
         var allObjectsSorted = allObjects.OrderBy(o => o.Key);
 
         var currentProgressObjectName = $"{prefix}pathling-s3-importer-last-imported.txt";
 
+        log.LogInformation(
+            "Name of the current progress tracker object set to {CurrentProgressObjectName}.",
+            currentProgressObjectName
+        );
+
         var stopwatch = new Stopwatch();
         var importedCount = 0;
-        using (log.BeginScope("[importing ndjson]"))
-        {
-            foreach (var item in allObjectsSorted)
-            {
-                var objectUrl = $"s3://{S3BucketName}/{item.Key}";
 
+        foreach (var item in allObjectsSorted)
+        {
+            var objectUrl = $"s3://{S3BucketName}/{item.Key}";
+
+            using (log.BeginScope("[Importing ndjson file {NdjsonObjectUrl}]", objectUrl))
+            {
                 var parameter = new Parameters.ParameterComponent()
                 {
                     Name = "source",
@@ -201,54 +226,55 @@ public class ImportCliCommand
                 // we might want to add multiple ndjson files at once in batches.
                 importParameters.Parameter.Add(parameter);
 
-                using (log.BeginScope($"[importing {objectUrl}]"))
+                log.LogInformation("{ImportParameters}", importParameters.ToJson());
+
+                log.LogInformation(
+                    "Starting {PathlingServerBaseUrl}/$import for {ObjectUrl}",
+                    PathlingServerBaseUrl,
+                    objectUrl
+                );
+
+                if (!IsDryRun)
                 {
-                    log.LogInformation("{ImportParameters}", importParameters.ToJson());
-
-                    log.LogInformation(
-                        "Starting {PathlingServerBaseUrl}/$import for {ObjectUrl}",
-                        PathlingServerBaseUrl,
-                        objectUrl
-                    );
-
-                    if (!IsDryRun)
+                    stopwatch.Restart();
+                    var response = await retryPipeline.ExecuteAsync(async token =>
                     {
-                        stopwatch.Restart();
-                        var response = await retryPipeline.ExecuteAsync(async token =>
-                        {
-                            return await fhirClient.WholeSystemOperationAsync(
-                                "import",
-                                importParameters
-                            );
-                        });
-                        stopwatch.Stop();
-
-                        log.LogInformation("{ImportResponse}", response.ToJson());
-                        log.LogInformation("Import took {ImportDuration}", stopwatch.Elapsed);
-
-                        var bytes = Encoding.UTF8.GetBytes(objectUrl);
-                        using var memoryStream = new MemoryStream(bytes);
-
-                        // persist progress
-                        var putArgs = new PutObjectArgs()
-                            .WithBucket(S3BucketName)
-                            .WithObject(currentProgressObjectName)
-                            .WithContentType("text/plain")
-                            .WithStreamData(memoryStream)
-                            .WithObjectSize(bytes.LongLength);
-
-                        await retryPipeline.ExecuteAsync(async token =>
-                        {
-                            await minio.PutObjectAsync(putArgs, token);
-                        });
-                    }
-                    else
-                    {
-                        log.LogInformation(
-                            "Running import in dry run mode. Waiting a few seconds."
+                        return await fhirClient.WholeSystemOperationAsync(
+                            "import",
+                            importParameters
                         );
-                        await Task.Delay(TimeSpan.FromSeconds(5));
-                    }
+                    });
+                    stopwatch.Stop();
+
+                    log.LogInformation("{ImportResponse}", response.ToJson());
+                    log.LogInformation("Import took {ImportDuration}", stopwatch.Elapsed);
+
+                    var bytes = Encoding.UTF8.GetBytes(objectUrl);
+                    using var memoryStream = new MemoryStream(bytes);
+
+                    // persist progress
+                    var putArgs = new PutObjectArgs()
+                        .WithBucket(S3BucketName)
+                        .WithObject(currentProgressObjectName)
+                        .WithContentType("text/plain")
+                        .WithStreamData(memoryStream)
+                        .WithObjectSize(bytes.LongLength);
+
+                    stopwatch.Restart();
+                    await retryPipeline.ExecuteAsync(async token =>
+                    {
+                        await minio.PutObjectAsync(putArgs, token);
+                    });
+                    stopwatch.Stop();
+                    log.LogInformation(
+                        "Persisting progress took {PutProgressDuration}",
+                        stopwatch.Elapsed
+                    );
+                }
+                else
+                {
+                    log.LogInformation("Running import in dry run mode. Waiting a few seconds.");
+                    await Task.Delay(TimeSpan.FromSeconds(5));
                 }
 
                 importedCount++;
