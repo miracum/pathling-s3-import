d48cb918dcdd2e12cd90dfbcd6a27e21c5122c25
chgl <chgl@users.noreply.github.com>
2024-02-24 16:36:48 +0100

    fix: improved logging and added e2e tests (#7)
    * feat: improved logging
    
    * test(e2e): added e2e tests
    
    * chore: formatted

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # pathling-s3-import
 
+[![OpenSSF Scorecard](https://api.securityscorecards.dev/projects/github.com/miracum/pathling-s3-import/badge)](https://api.securityscorecards.dev/projects/github.com/miracum/pathling-s3-import)
+[![SLSA 3](https://slsa.dev/images/gh-badge-level3.svg)](https://slsa.dev)
+
 Tool for automatically [$import'ing](https://pathling.csiro.au/docs/server/operations/import) lists of ndjson FHIR resources from an S3-compatible bucket into a Pathling server.
 
 ## Usage
@@ -8,4 +11,32 @@ See the help text of the command by simply running:
 
 ```sh
 docker run --rm -it ghcr.io/miracum/pathling-s3-import:v1.1.1
+```
+
+## Development
+
+Launch development fixtures:
+
+```sh
+docker compose up
+```
+
+Install dependencies
+
+```sh
+dotnet restore
+dotnet tool restore
+```
+
+Start the tool
+
+```sh
+dotnet run --project src/PathlingS3Import/ -- \
+    --s3-endpoint=http://localhost:9000 \
+    --pathling-server-base-url=http://localhost:8082/fhir \
+    --s3-access-key=admin \
+    --s3-secret-key=miniopass \
+    --s3-bucket-name=fhir \
+    --s3-object-name-prefix=staging/ \
+    --dry-run=false
 ```
---
+++ src/PathlingS3Import.Tests.E2E/Test.cs
@@ -0,0 +1,81 @@
+using DotNet.Testcontainers.Builders;
+using FluentAssertions;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using Xunit.Abstractions;
+using Task = System.Threading.Tasks.Task;
+
+namespace PathlingS3Import.Tests.E2E;
+
+public class Tests(ITestOutputHelper output)
+{
+    private readonly ITestOutputHelper output = output;
+
+    [Fact]
+    public async Task StartImportTool_WithRunningPathlingServerAndMinio_ShouldCreateExpectedNumberOfResources()
+    {
+        // this test requires the dev fixtures to be running on their default ports as well as
+        // a PathlingS3Import image to exist.
+
+        using var stdoutStream = new MemoryStream();
+        using var stderrStream = new MemoryStream();
+        using var consumer = Consume.RedirectStdoutAndStderrToStream(stdoutStream, stderrStream);
+
+        var pathlingServerBaseUrl = "http://host.docker.internal:8082/fhir";
+        var resourceType = ResourceType.Patient;
+
+        string[] args =
+        [
+            "--s3-endpoint=http://host.docker.internal:9000",
+            $"--pathling-server-base-url={pathlingServerBaseUrl}",
+            "--s3-access-key=admin",
+            "--s3-secret-key=miniopass",
+            "--s3-bucket-name=fhir",
+            "--s3-object-name-prefix=staging/",
+            $"--import-resource-type={resourceType}",
+            "--dry-run=false"
+        ];
+
+        var testImageTag =
+            Environment.GetEnvironmentVariable("PATHLING_S3_IMPORT_IMAGE_TAG") ?? "test";
+
+        var testContainer = new ContainerBuilder()
+            .WithImage($"ghcr.io/miracum/pathling-s3-import:{testImageTag}")
+            .WithCommand(args)
+            .WithOutputConsumer(consumer)
+            .WithExtraHost("host.docker.internal", "host-gateway")
+            .Build();
+
+        await testContainer.StartAsync();
+
+        var exitCode = await testContainer.GetExitCodeAsync();
+
+        output.WriteLine("Test container exited");
+
+        consumer.Stdout.Seek(0, SeekOrigin.Begin);
+        using var stdoutReader = new StreamReader(consumer.Stdout);
+        var stdout = stdoutReader.ReadToEnd();
+        output.WriteLine(stdout);
+
+        exitCode.Should().Be(0);
+
+        // use a different base URL since this test isn't run inside
+        // a container. Slightly ugly.
+        using var fhirClient = new FhirClient(
+            "http://localhost:8082/fhir",
+            settings: new()
+            {
+                PreferredFormat = ResourceFormat.Json,
+                Timeout = (int)TimeSpan.FromSeconds(60).TotalMilliseconds
+            }
+        );
+
+        var response = await fhirClient.SearchAsync(
+            resourceType.ToString(),
+            summary: SummaryType.Count
+        );
+
+        response.Should().NotBeNull();
+        response!.Total.Should().Be(3);
+    }
+}
